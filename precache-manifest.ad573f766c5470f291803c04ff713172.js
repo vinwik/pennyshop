04ed08dd6a7773d9163a635f800615b6cf1a0043
vinwik <vinwik@hotmail.com>
2020-02-22 18:41:11 +0000

    Updates

---
+++ precache-manifest.ad573f766c5470f291803c04ff713172.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f290953d86843e760250394338da8b",
+    "url": "/pennyshop/index.html"
+  },
+  {
+    "revision": "5f7de2e354336fc10e12",
+    "url": "/pennyshop/static/css/main.65d2b46a.chunk.css"
+  },
+  {
+    "revision": "f4c751c3237b5ca6d21a",
+    "url": "/pennyshop/static/js/2.7ad562c5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pennyshop/static/js/2.7ad562c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7de2e354336fc10e12",
+    "url": "/pennyshop/static/js/main.49501a35.chunk.js"
+  },
+  {
+    "revision": "42d288d592f02f9c9462",
+    "url": "/pennyshop/static/js/runtime-main.847ade5e.js"
+  },
+  {
+    "revision": "0cdd6edbebf98f4f9574d3bfc6e49fec",
+    "url": "/pennyshop/static/media/404.0cdd6edb.jpg"
+  },
+  {
+    "revision": "a14598e0f7754bc71f2e8c9a8f6cab1d",
+    "url": "/pennyshop/static/media/minus-square.a14598e0.svg"
+  },
+  {
+    "revision": "3703a6722e42c61c004f2d7945d44d24",
+    "url": "/pennyshop/static/media/plus-square.3703a672.svg"
+  },
+  {
+    "revision": "7cfd9ae538e2ce57c297d159c398a02c",
+    "url": "/pennyshop/static/media/pricedown-black.7cfd9ae5.woff"
+  },
+  {
+    "revision": "38971c3aabf2f19301d7d373e50ecec0",
+    "url": "/pennyshop/static/media/ps-logo.38971c3a.svg"
+  },
+  {
+    "revision": "24d28d043b515c864884e7ba72117dc5",
+    "url": "/pennyshop/static/media/shopping-cart.24d28d04.svg"
+  },
+  {
+    "revision": "aff71bea5f4ac847c446f278b72cb057",
+    "url": "/pennyshop/static/media/trash.aff71bea.svg"
+  }
+]);
