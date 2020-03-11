9cf3749333f6ebbd40d63a8ed9dc74342dfceb12
vinwik <vinwik@hotmail.com>
2020-03-11 09:36:27 +0000

    moved slick-theme.css from node modules

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ import { Link } from "react-router-dom";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "../slick-theme.css";
 
 const Home = () => {
   const settings = {
