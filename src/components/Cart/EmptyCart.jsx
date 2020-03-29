0a52876eca1d6925e4712dd9235b2cd425508d09
vinwik <vinwik@hotmail.com>
2020-03-29 23:21:21 +0100

    empty cart section improved

--- src/components/Cart/EmptyCart.jsx
+++ src/components/Cart/EmptyCart.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function EmptyCart() {
   return (
     <div className="empty-cart">
-      <h1>Your shopping cart is empty</h1>
+      <h1>shopping cart</h1>
+      <h2>Your cart is empty</h2>
+      <button className="light-btn">
+        <Link to="/shop">Shop now</Link>
+      </button>
     </div>
   );
 }
