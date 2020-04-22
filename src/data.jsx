d2e85385494dbd7b5110727c65ddd840e1c3848d
vinwik <vinwik@hotmail.com>
2020-04-23 00:52:01 +0100

    Updated product list

--- src/data.jsx
+++ src/data.jsx
@@ -12,25 +12,10 @@ export const storeProducts = [
     info: `The power of the dark side compels you to skate this 22" deck built with all-black trucks, and wheels to match the Sith Lord’s very own lightsaber. Darth Vader is the most feared man in the galaxy but even he has some good left inside him. Take this board out for a spin and you too will see the light!`,
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 2,
-    title: `Ice 22"`,
-    img: "./assets/small/ice-22.jpg",
-    imgLarge: "./assets/ice-22.jpg",
-    size: `22"`,
-    price: 89.99,
-    collection: "Classic",
-    tags: [`22"`, "Classic"],
-    likes: 746,
-    info: `What's cooler than being cool? Riding the new Penny Ice! This frosty deck is sure to keep you chilled on a hot day. With matching translucent wheels and custom navy trucks, the 22” Ice is one of our new favorites!`,
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 3,
     title: `Darth Vader 27"`,
     img: "./assets/small/vader-27.jpg",
     imgLarge: "./assets/vader-27.jpg",
@@ -42,7 +27,22 @@ export const storeProducts = [
     info: `The power of the dark side compels you to skate this 22" deck built with all-black trucks, and wheels to match the Sith Lord’s very own lightsaber. Darth Vader is the most feared man in the galaxy but even he has some good left inside him. Take this board out for a spin and you too will see the light!`,
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
+  },
+  {
+    id: 3,
+    title: `Ice 22"`,
+    img: "./assets/small/ice-22.jpg",
+    imgLarge: "./assets/ice-22.jpg",
+    size: `22"`,
+    price: 89.99,
+    collection: "Classic",
+    tags: [`22"`, "Classic"],
+    likes: 746,
+    info: `What's cooler than being cool? Riding the new Penny Ice! This frosty deck is sure to keep you chilled on a hot day. With matching translucent wheels and custom navy trucks, the 22” Ice is one of our new favorites!`,
+    inCart: false,
+    count: 0,
+    total: 0,
   },
   {
     id: 4,
@@ -57,68 +57,100 @@ export const storeProducts = [
     info: `What's cooler than being cool? Riding the new Penny Ice! This frosty deck is sure to keep you chilled on a hot day. With matching translucent wheels and custom navy trucks, the 27” Ice is one of our new favorites!`,
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 5,
-    title: `Darth Vader 22"`,
-    img: "./assets/small/vader-22.jpg",
-    imgLarge: "./assets/vader-22.jpg",
+    title: `Boba Fett 22"`,
+    img: "./assets/small/boba-22.jpg",
+    imgLarge: "./assets/boba-22.jpg",
     size: `22"`,
     price: 89.99,
     collection: "Star Wars",
     tags: [`22"`, "Star Wars"],
     likes: 2350,
-    info: `The power of the dark side compels you to skate this 22" deck built with all-black trucks, and wheels to match the Sith Lord’s very own lightsaber. Darth Vader is the most feared man in the galaxy but even he has some good left inside him. Take this board out for a spin and you too will see the light!`,
+    info: `One of the most loved and mysterious characters from the Star Wars galaxy is the Imperial bounty hunter, Boba Fett. His counter-culture lifestyle and weathered appearance makes him the ideal renegade to be featured on this skateboard. On your next hunt for Captain Solo, grab this 22” board built on all-black trucks and yellow wheels. Paired with the matching green deck this board flawlessly embodies the bounty hunter’s intimidating look.`,
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
     id: 6,
-    title: `Ice 22"`,
-    img: "./assets/small/ice-22.jpg",
-    imgLarge: "./assets/ice-22.jpg",
-    size: `22"`,
-    price: 89.99,
-    collection: "Classic",
-    tags: [`22"`, "Classic"],
-    likes: 864,
-    info: `What's cooler than being cool? Riding the new Penny Ice! This frosty deck is sure to keep you chilled on a hot day. With matching translucent wheels and custom navy trucks, the 22” Ice is one of our new favorites!`,
-    inCart: false,
-    count: 0,
-    total: 0
-  },
-  {
-    id: 7,
-    title: `Darth Vader 27"`,
-    img: "./assets/small/vader-27.jpg",
-    imgLarge: "./assets/vader-27.jpg",
+    title: `Boba Fett 27"`,
+    img: "./assets/small/boba-27.jpg",
+    imgLarge: "./assets/boba-27.jpg",
     size: `27"`,
     price: 99.99,
     collection: "Star Wars",
     tags: [`27"`, "Star Wars"],
     likes: 304,
-    info: `The power of the dark side compels you to skate this 22" deck built with all-black trucks, and wheels to match the Sith Lord’s very own lightsaber. Darth Vader is the most feared man in the galaxy but even he has some good left inside him. Take this board out for a spin and you too will see the light!`,
+    info: `One of the most loved and mysterious characters from the Star Wars galaxy is the Imperial bounty hunter, Boba Fett. His counter-culture lifestyle and weathered appearance makes him the ideal renegade to be featured on this skateboard. On your next hunt for Captain Solo, grab this 27” board built on all-black trucks and yellow wheels. Paired with the matching green deck this board flawlessly embodies the bounty hunter’s intimidating look.`,
     inCart: false,
     count: 0,
-    total: 0
+    total: 0,
   },
   {
+    id: 7,
+    title: `R2-D2 22"`,
+    img: "./assets/small/r2-22.jpg",
+    imgLarge: "./assets/r2-22.jpg",
+    size: `22"`,
+    price: 89.99,
+    collection: "Star Wars",
+    tags: [`22"`, "Star Wars"],
+    likes: 864,
+    info: `Everyone’s favorite Droid is back on this 22” skateboard! Whether piloting an X-wing or serving drinks in Jabba’s palace, R2-D2 has always been a reliable counterpart. Well, this board is no different. The blue deck and wheels, paired with black and blue trucks make this skateboard nothing short of interstellar; and you may just find yourself a new best friend!`,
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+
+  {
     id: 8,
-    title: `Ice 27"`,
-    img: "./assets/small/ice-27.jpg",
-    imgLarge: "./assets/ice-27.jpg",
+    title: `Storm Trooper 27"`,
+    img: "./assets/small/trooper-27.jpg",
+    imgLarge: "./assets/trooper-27.jpg",
+    size: `27"`,
+    price: 99.99,
+    collection: "Star Wars",
+    tags: [`27"`, "Star Wars"],
+    likes: 54,
+    info: `Loyalty to the Empire is a Stormtroopers first prerogative; second would be to roll nice and smooth under your feet! This 27” warrior is locked and loaded with black and white trucks, and navy blue wheels to match the deck. The clean look is reminiscent of a freshly polished Stormtrooper in line to see the Emperor. While skating this board around just be careful to not yell “Rebel Scum” at passing pedestrians!`,
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 9,
+    title: `Bart Simpson 22"`,
+    img: "./assets/small/bart-22.jpg",
+    imgLarge: "./assets/bart-22.jpg",
+    size: `22"`,
+    price: 89.99,
+    collection: "Simpsons",
+    tags: [`22"`, "Simpsons"],
+    likes: 864,
+    info: `Cause mischief around Springfield on the 22" one-of-a kind- board. Looking for a premium plastic skateboard? Look no further than our Limited Edition 22" El Barto Board. This board features an all white deck with blue base plates, orange trucks and white wheels matching the El Barto graphic perfectly.`,
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+
+  {
+    id: 10,
+    title: `Ollieve 27"`,
+    img: "./assets/small/ollieve-27.jpg",
+    imgLarge: "./assets/ollieve-27.jpg",
     size: `27"`,
     price: 99.99,
     collection: "Classic",
-    tags: [`27"`, "Classic"],
+    tags: [`27"`, "Classics"],
     likes: 54,
-    info: `What's cooler than being cool? Riding the new Penny Ice! This frosty deck is sure to keep you chilled on a hot day. With matching translucent wheels and custom navy trucks, the 27” Ice is one of our new favorites!`,
+    info: `The concrete jungle can be overwhelming without a trusty mode of transportation. Escape the hustle and bustle with the new Penny Sage. The 27” deck and matching translucent wheels will cleanse your soul in a concrete world!`,
     inCart: false,
     count: 0,
-    total: 0
-  }
+    total: 0,
+  },
 ];
 
 export const detailProduct = {
@@ -132,26 +164,26 @@ export const detailProduct = {
   info: `The power of the dark side compels you to skate this 22" deck built with all-black trucks, and wheels to match the Sith Lord’s very own lightsaber. Darth Vader is the most feared man in the galaxy but even he has some good left inside him. Take this board out for a spin and you too will see the light!`,
   inCart: false,
   count: 0,
-  total: 0
+  total: 0,
 };
 
 export const detailArticle = [
   {
     title: `22" PENNY SKATEBOARD`,
-    description: `The first and original 22” Penny Skateboard is a quick and nimble ride that’s perfect for getting from place to place quickly and with no fuss at all. Our 22” Penny is a great size for skaters looking for easy transportation and fits perfectly in our Penny Pouch Backpacks when you’re not skating. It’s also great for those younger Penny riders who are on the smaller side. If you are buying a gift for a younger rider or just like the portable size, than the 22” is for you.`
+    description: `The first and original 22” Penny Skateboard is a quick and nimble ride that’s perfect for getting from place to place quickly and with no fuss at all. Our 22” Penny is a great size for skaters looking for easy transportation and fits perfectly in our Penny Pouch Backpacks when you’re not skating. It’s also great for those younger Penny riders who are on the smaller side. If you are buying a gift for a younger rider or just like the portable size, than the 22” is for you.`,
   },
   {
     title: `27" PENNY SKATEBOARD`,
-    description: `For those who like a little more plastic under their feet, the Penny 27” skateboard is the one for you. Whether you’re an experienced skater looking for a fun and easy ride or you’re new to the game and are trying skateboarding for the first time, the 27” it is the ultimate all round skateboard. Commonly referred to as the “Nickel,” the 27” boasts the same great features as the 22” however its larger deck is great for perfecting new skate tricks or beginners looking for a little more stability.`
+    description: `For those who like a little more plastic under their feet, the Penny 27” skateboard is the one for you. Whether you’re an experienced skater looking for a fun and easy ride or you’re new to the game and are trying skateboarding for the first time, the 27” it is the ultimate all round skateboard. Commonly referred to as the “Nickel,” the 27” boasts the same great features as the 22” however its larger deck is great for perfecting new skate tricks or beginners looking for a little more stability.`,
   },
   {
     title: `CLASSIC`,
     description: `Distinguished by its plastic deck, the Classic Penny Skateboard is available in a variety of colours and designs.
-    It will be your best companion for your barefoot, particularly during the summer.`
+    It will be your best companion for your barefoot, particularly during the summer.`,
   },
   {
     title: `STAR WARS COLLECTION`,
     description: `Not so long ago… in a skatepark not so far away… Penny Skateboards and Star Wars combined forces to bring the galaxy a collaboration like none before it. By taking the likenesses of some of the most beloved characters from the Star Wars universe, Penny has created a line up of 6 collectable skateboards.
-    In this line there is a board for every side of The force. You can roll on the dark side with Darth Vader and his loyal army of Stormtroopers; or you could side with the Rebels and hop on 4 wheels with your best pal R2-D2. And if you don’t like to pick sides, then blast off into neutral air space with the notorious bounty hunter, Boba Fett. These boards are now available globally and beyond. Ride safe young padawans!`
-  }
+    In this line there is a board for every side of The force. You can roll on the dark side with Darth Vader and his loyal army of Stormtroopers; or you could side with the Rebels and hop on 4 wheels with your best pal R2-D2. And if you don’t like to pick sides, then blast off into neutral air space with the notorious bounty hunter, Boba Fett. These boards are now available globally and beyond. Ride safe young padawans!`,
+  },
 ];
